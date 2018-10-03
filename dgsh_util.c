45b74972cb724b04618e4467966496f710ba6f96
Marios Fragkoulis <mfg@aueb.gr>
2018-10-04 01:33:14 +0200

    Work in progress with identifying assembly file on Mac

--- dgsh_util.c
+++ dgsh_util.c
@@ -20,7 +20,9 @@
 
 #include <ctype.h>
 #ifdef __APPLE__
-#include <libelf/libelf.h>
+#include <mach-o/loader.h>
+#include <mach-o/swap.h>
+#include <math.h>
 #else
 #include <elf.h>
 #endif
@@ -36,6 +38,183 @@
 
 #define MAX_LINE_LEN 1024
 
+#ifdef __APPLE__
+
+void *
+load_bytes(FILE *obj_file, int offset, int size)
+{
+  void *buf = calloc(1, size);
+  fseek(obj_file, offset, SEEK_SET);
+  fread(buf, size, 1, obj_file);
+  return buf;
+}
+
+void
+dump_sections(FILE *obj_file, uint32_t type, int offset, int is_swap, uint32_t nsects)
+{
+  int actual_offset = offset;
+  fprintf(stderr, "%s: %d sections at offset: %d\n", __func__, nsects, offset);
+  for (int  i = 0; i < nsects; i++) {
+    if (type == LC_SEGMENT_64) {
+      int struct_size = sizeof(struct section_64);
+      struct section_64 *sect = load_bytes(obj_file, actual_offset,
+                                           struct_size);
+      if (is_swap) {
+        fprintf(stderr, "swap section64\n");
+        swap_section_64(sect, nsects, 0);
+      }
+      fprintf(stderr, "section64 %d: size %d, struct_size: %d, file offset: %d, align: %d\n", i,
+                       sect->size, struct_size, sect->offset, sect->align);
+
+      fprintf(stderr, "section64 %d: segname: %s\n", i, sect->segname);
+      fprintf(stderr, "section64 %d: sectname: %s\n", i, sect->sectname);
+      fprintf(stderr, "section64 %d: addr: %lx\n", i, sect->addr);
+      //fprintf(stderr, "section64 %d: contents: %s\n", i, (char *)(sect->addr + sect->offset));
+      char *b = load_bytes(obj_file, sect->offset, sect->size);
+      fprintf(stderr, "section64 %d: contents: %s\n", i, b);
+
+      actual_offset += struct_size;
+      free(sect);
+    } else if (type == LC_SEGMENT) {
+      int struct_size = sizeof(struct section);
+      struct section *sect = load_bytes(obj_file, actual_offset, struct_size);
+      if (is_swap) {
+        fprintf(stderr, "swap section\n");
+        swap_section(sect, nsects, 0);
+      }
+      fprintf(stderr, "section %d: size %d, struct_size: %d, file offset: %d, align: %d\n", i,
+                       sect->size, struct_size, sect->offset, sect->align);
+
+      fprintf(stderr, "segname: %s\n", sect->segname);
+      fprintf(stderr, "sectname: %s\n", sect->sectname);
+
+      actual_offset += struct_size;
+      free(sect);
+    }
+  }
+}
+
+void
+dump_segment_commands(FILE *obj_file, int offset, int is_swap, uint32_t ncmds)
+{
+  int actual_offset = offset;
+  fprintf(stderr, "ncmds: %d\n", ncmds);
+  for (int  i = 0; i < ncmds; i++) {
+    struct load_command *cmd = load_bytes(obj_file, actual_offset,
+                                          sizeof(struct load_command));
+    if (is_swap) {
+      swap_load_command(cmd, 0);
+    }
+
+    if (cmd->cmd == LC_SEGMENT_64) {
+      int struct_size = sizeof(struct segment_command_64);
+      struct segment_command_64 *segment = load_bytes(obj_file, actual_offset,
+                                                      struct_size);
+      if (is_swap) {
+        swap_segment_command_64(segment, 0);
+      }
+
+      fprintf(stderr, "segname: %s, size: %d\n", segment->segname, segment->cmdsize);
+      dump_sections(obj_file, cmd->cmd, actual_offset + struct_size, is_swap,
+                    segment->nsects);
+      fprintf(stderr, "cmdsize: %d, segment command size: %lu, sections: %d\n",
+              cmd->cmdsize, sizeof(struct segment_command_64), segment->nsects);
+
+      free(segment);
+    } else if (cmd->cmd == LC_SEGMENT) {
+      int struct_size = sizeof(struct segment_command);
+      struct segment_command *segment = load_bytes(obj_file, actual_offset,
+                                                   struct_size);
+      if (is_swap) {
+        swap_segment_command(segment, 0);
+      }
+
+      fprintf(stderr, "segname: %s\n", segment->segname);
+      dump_sections(obj_file, cmd->cmd, actual_offset + struct_size, is_swap,
+                    segment->nsects);
+      fprintf(stderr, "cmdsize: %d, segment command size: %lu, sections: %d\n",
+              cmd->cmdsize, sizeof(struct segment_command), segment->nsects);
+
+      free(segment);
+    }
+
+    actual_offset += cmd->cmdsize;
+
+    free(cmd);
+  }
+}
+
+void
+dump_mach_header(FILE *obj_file, int offset, int is_64, int is_swap)
+{
+  uint32_t ncmds;
+  int load_commands_offset = offset;
+
+  if (is_64) {
+    int header_size = sizeof(struct mach_header_64);
+    struct mach_header_64 *header = load_bytes(obj_file, offset, header_size);
+    if (is_swap) {
+      swap_mach_header_64(header, 0);
+    }
+    ncmds = header->ncmds;
+    load_commands_offset += header_size;
+
+    free(header);
+  } else {
+    int header_size = sizeof(struct mach_header);
+    struct mach_header *header = load_bytes(obj_file, offset, header_size);
+    if (is_swap) {
+      swap_mach_header(header, 0);
+    }
+
+    ncmds = header->ncmds;
+    load_commands_offset += header_size;
+
+    free(header);
+  }
+
+  dump_segment_commands(obj_file, load_commands_offset, is_swap, ncmds);
+}
+
+int should_swap_bytes(uint32_t magic) {
+  return magic == MH_CIGAM || magic == MH_CIGAM_64;
+}
+
+int
+is_magic_64(uint32_t magic)
+{
+  return magic == MH_MAGIC_64 || magic == MH_CIGAM_64;
+}
+
+uint32_t
+read_magic(FILE *obj_file, int offset)
+{
+  uint32_t magic;
+  fseek(obj_file, offset, SEEK_SET);
+  fread(&magic, sizeof(uint32_t), 1, obj_file);
+  return magic;
+}
+
+void
+dump_segments(FILE *obj_file)
+{
+  uint32_t magic = read_magic(obj_file, 0);
+  int is_64 = is_magic_64(magic);
+  int is_swap = should_swap_bytes(magic);
+  dump_mach_header(obj_file, 0, is_64, is_swap);
+}
+
+int
+is_dgsh_program(const char *path)
+{
+  FILE *obj_file = fopen(path, "rb");
+  dump_segments(obj_file);
+  fclose(obj_file);
+
+  return 0;
+}
+
+#else
 
 /*
  * Return true if the provided ELF data contain a DGSH note section
@@ -177,6 +356,8 @@ is_dgsh_program(const char *path)
   munmap(data, file_size);
   return r;
 }
+#endif /* __APPLE__ */
+
 
 #ifdef DGSH_COMPAT
 int
